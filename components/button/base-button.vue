01fe9970081d028ee5eae580a3f359493a900047
Bngoctam95 <105120278+Bngoctam95@users.noreply.github.com>
2023-04-04 08:39:10 +0700

    Create pricing layout

---
+++ components/button/base-button.vue
@@ -0,0 +1,53 @@
+<template>
+  <button
+    class="button"
+    :disabled="disabled"
+    @click="handleClick"
+  >
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'BaseButton',
+  props: {
+    disabled: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {
+    handleClick () {
+      this.$emit('click')
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+  .button {
+    color: var(--primary-color);
+    font-size: 2rem;
+    padding: 15px 45px;
+    border: none;
+    border-radius: 50px;
+    cursor: pointer;
+    background-color: white;
+    &:hover {
+      background-color: #dddddd;
+    }
+    &:active {
+      background-color: white;
+    }
+  }
+  .primary {
+    color: white;
+    background-color: var(--primary-color);
+    &:hover {
+      background-color: #0a82f1;
+    }
+    &:active {
+      background-color: var(--primary-color);
+    }
+  }
+</style>
--- components/pages/home/pricing-section.vue
+++ components/pages/home/pricing-section.vue
@@ -1,13 +1,248 @@
 <template>
-  <h1>Pricing</h1>
+  <div class="wrapper">
+    <div class="pricing">
+      <div class="pricing__content">
+        <h2>Pricing</h2>
+        <p>
+          Most calendars are designed for teams.<br>
+          Slate is designed for freelancers
+        </p>
+      </div>
+      <div class="pricing__order">
+        <div class="pricing__order__free">
+          <h2 class="title">
+            Free
+          </h2>
+          <p class="subtitle">
+            Organize across <br>
+            all apps by hand
+          </p>
+          <div class="price">
+            <p class="price__number">
+              0
+            </p>
+            <div class="price__mth">
+              <span>$</span>
+              <p>Per Month</p>
+            </div>
+          </div>
+          <div class="feature">
+            Pricing Feature
+          </div>
+          <div class="feature">
+            Pricing Feature
+          </div>
+          <div class="feature">
+            Pricing Feature
+          </div>
+          <div class="feature">
+            Pricing Feature
+          </div>
+          <div class="feature">
+            Pricing Feature
+          </div>
+          <baseButton class="primary btn">
+            Order Now
+          </baseButton>
+        </div>
+        <div class="pricing__order__free">
+          <h2 class="title">
+            Free
+          </h2>
+          <p class="subtitle">
+            Organize across <br>
+            all apps by hand
+          </p>
+          <div class="price">
+            <p class="price__number">
+              0
+            </p>
+            <div class="price__mth">
+              <span>$</span>
+              <p>Per Month</p>
+            </div>
+          </div>
+          <div class="feature">
+            Pricing Feature
+          </div>
+          <div class="feature">
+            Pricing Feature
+          </div>
+          <div class="feature">
+            Pricing Feature
+          </div>
+          <div class="feature">
+            Pricing Feature
+          </div>
+          <div class="feature">
+            Pricing Feature
+          </div>
+          <baseButton class="primary btn">
+            Order Now
+          </baseButton>
+        </div>
+        <div class="pricing__order__free">
+          <h2 class="title">
+            Free
+          </h2>
+          <p class="subtitle">
+            Organize across <br>
+            all apps by hand
+          </p>
+          <div class="price">
+            <p class="price__number">
+              0
+            </p>
+            <div class="price__mth">
+              <span>$</span>
+              <p>Per Month</p>
+            </div>
+          </div>
+          <div class="feature">
+            Pricing Feature
+          </div>
+          <div class="feature">
+            Pricing Feature
+          </div>
+          <div class="feature">
+            Pricing Feature
+          </div>
+          <div class="feature">
+            Pricing Feature
+          </div>
+          <div class="feature">
+            Pricing Feature
+          </div>
+          <baseButton class="primary btn">
+            Order Now
+          </baseButton>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import baseButton from '@/components/button/base-button.vue'
 export default {
-  name: 'PricingSection'
+  name: 'PricingSection',
+  components: {
+    baseButton
+  }
 }
 </script>
 
 <style lang="scss" scoped>
+  .wrapper {
+    width: 100%;
+    background-color: rgba(37, 43, 66, 1);
+    .pricing {
+      width: 1210px;
+      max-width: 100%;
+      margin: 0 auto;
+      padding: 110px 50px;
 
+      &__content {
+        text-align: center;
+        font-weight: 400;
+        color: #FFFFFF;
+        letter-spacing: 0.2px;
+        padding-bottom: 80px;
+        h2 {
+          font-size: 4.8rem;
+          line-height: 55px;
+          margin-bottom: 20px;
+        }
+
+        p {
+          font-size: 2.8rem;
+          line-height: 40px;
+        }
+      }
+
+      &__order {
+        display: flex;
+        justify-content: space-around;
+
+        &__free {
+          width: 335px;
+          padding: 40px;
+          text-align: center;
+          background-color: white;
+          border: 1px solid #DEDEDE;
+          box-shadow: 0px 13px 19px rgba(0, 0, 0, 0.07);
+          border-radius: 10px;
+
+          .title {
+            font-family: 'Graphik-Medium';
+            font-weight: 700;
+            font-size: 2rem;
+            line-height: 28px;
+            letter-spacing: 0.1px;
+            text-transform: uppercase;
+            color: #252B42;
+          }
+
+          .subtitle {
+            font-weight: 400;
+            font-size: 1.6rem;
+            line-height: 23px;
+            letter-spacing: 0.1px;
+            color: #374754;
+          }
+
+          .price {
+            padding: 30px 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            gap: 10px;
+
+            &__number {
+              font-family: 'Graphik-Medium';
+              font-weight: 700;
+              font-size: 7.4rem;
+              line-height: 84px;
+              letter-spacing: 0.2px;
+              color: #252B42;
+            }
+
+            &__mth {
+              text-align: left;
+              padding-top: 10px;
+              span {
+                font-family: 'Graphik-Medium';
+                font-weight: 700;
+                font-size: 2rem;
+                line-height: 28px;
+                letter-spacing: 0.1px;
+                color: #252B42;
+              }
+              p {
+                font-family: 'Graphik-Light';
+                font-weight: 400;
+                font-size: 1.6rem;
+                line-height: 23px;
+                letter-spacing: 0.1px;
+                color: #374754;
+              }
+            }
+          }
+
+          .feature {
+            padding: 10px;
+            font-weight: 400;
+            font-size: 1.5rem;
+            line-height: 28px;
+            letter-spacing: 0.2px;
+            color: #252B42;
+          }
+
+          .btn {
+            margin-top: 30px;
+          }
+        }
+      }
+    }
+  }
 </style>
